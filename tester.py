a290133146828d2b2299ad68fd39da01a84abc42
tenacke <emrekilic1010@gmail.com>
2025-03-26 15:12:27 +0300

    Bug fix

--- tester.py
+++ tester.py
@@ -14,7 +14,7 @@ datasets_path = os.path.join(os.path.dirname(__file__), "datasets")
 csv_files_path = os.path.join(os.path.dirname(__file__), "csv")
 
 # Check if command line arguments are provided
-if len(sys.argv) < 4:
+if len(sys.argv) < 5:
     print(
         "Usage: python tester.py <model_name> [powerful|poor|average] <number_of_repetitions>"
     )
@@ -28,17 +28,13 @@ if sys.argv[1] == "--help":
 
 # Get the model name from the command line arguments
 model_name = sys.argv[1]
+if ":" not in model_name:
+    print(
+        "Invalid model name format. Please provide a valid model name. Example: evallm:v3"
+    )
+    sys.exit()
 model_base_name = model_name.split(":")[0]
 model_version = model_name.split(":")[1]
-coherence_model_name = model_base_name + "-coherence:" + model_version
-fluency_model_name = model_base_name + "-fluency:" + model_version
-relevance_model_name = model_base_name + "-relevance:" + model_version
-consistency_model_name = model_base_name + "-consistency:" + model_version
-if coherence_model_name not in get_models():
-    # if model_name not in get_models():
-    print("Invalid model name. Please provide a valid model name.")
-    sys.exit()
-
 
 # Get the evaluation type from the command line arguments
 evaluation_type = sys.argv[2]
@@ -50,6 +46,19 @@ if evaluation_type not in ["powerful", "poor", "average"]:
 
 if not sys.argv[3].isdigit():
     print("Invalid number of repetitions. Please provide a valid number.")
+    sys.exit()
+
+metric = sys.argv[4]
+if metric not in ["coherence", "fluency", "relevance", "consistency"]:
+    print(
+        "Invalid metric. Please provide one of the following: coherence, fluency, relevance, consistency"
+    )
+    sys.exit()
+
+model_name = f"{model_base_name}-{metric}:{model_version}"
+if model_name not in get_models():
+    # if model_name not in get_models():
+    print("Invalid model name. Please provide a valid model name.")
     sys.exit()
 
 number_of_repetitions = int(sys.argv[3])
@@ -68,7 +77,7 @@ exception_count = 0
 exception_file = open(f"{model_version}_{evaluation_type}_exceptions.txt", "w")
 
 # Evaluate the model
-results = pd.DataFrame(columns=["coherence", "fluency", "relevance", "consistency"])
+results = pd.DataFrame(columns=["result"])
 for index, row in test_data.iterrows():
     print(f"Evaluating index {index+1}...", flush=True)
 
@@ -78,85 +87,38 @@ for index, row in test_data.iterrows():
 
     query = "Summary\n" + row["decoded"] + "\n\nText\n" + text
 
-    repetition_results = {
-        "coherence": 0,
-        "fluency": 0,
-        "relevance": 0,
-        "consistency": 0,
-    }
+    repetition_results = {"result": 0}
     count = 0
     for i in range(number_of_repetitions):
         print(f"Repetition {i+1}...", flush=True)
         exception_ = False
-        coherence_response = client.generate(coherence_model_name, query).response
+        response = client.generate(model_name, query).response
         try:
-            coherence_response = coherence_response.split("</think>")[1]
-            coherence = coherence_response.split("Score: ")[1][0]
-            repetition_results["coherence"] += int(coherence)
-            coherence_count += 1
+            if "</think>" in response:
+                response = response.split("</think>")[1]
+            coherence = response.split("Score: ")[1][0]
+            repetition_results["result"] += int(coherence)
+            count += 1
         except:
-            print(f"Error parsing response index {index} repetition {i}", flush=True)
-            exception_file.write(
-                f'{index},{i},"{coherence_response.replace(",", ";")}"\n'
+            print(
+                f"Error parsing response index {index} repetition {i}",
+                flush=True,
             )
-            exception_ = True
-            exception_file.flush()
-
-        fluency_response = client.generate(fluency_model_name, query).response
-        try:
-            fluency_response = fluency_response.split("</think>")[1]
-            fluency = fluency_response.split("Score: ")[1][0]
-            repetition_results["fluency"] += int(fluency)
-            fluency_count += 1
-        except:
-            print(f"Error parsing response index {index} repetition {i}", flush=True)
-            exception_file.write(
-                f'{index},{i},"{fluency_response.replace(",", ";")}"\n'
-            )
-            exception_ = True
-            exception_file.flush()
-
-        relevance_response = client.generate(relevance_model_name, query).response
-        try:
-            relevance_response = relevance_response.split("</think>")[1]
-            relevance = relevance_response.split("Score: ")[1][0]
-            repetition_results["relevance"] += int(relevance)
-            relevance_count += 1
-        except:
-            print(f"Error parsing response index {index} repetition {i}", flush=True)
-            exception_file.write(
-                f'{index},{i},"{relevance_response.replace(",", ";")}"\n'
-            )
-            exception_ = True
-            exception_file.flush()
-
-        consistency_response = client.generate(consistency_model_name, query).response
-        try:
-            consistency_response = consistency_response.split("</think>")[1]
-            consistency = consistency_response.split("Score: ")[1][0]
-            repetition_results["consistency"] += int(consistency)
-            consistency_count += 1
-        except:
-            print(f"Error parsing response index {index} repetition {i}", flush=True)
-            exception_file.write(
-                f'{index},{i},"{consistency_response.replace(",", ";")}"\n'
-            )
+            exception_file.write(f'{index},{i},"{response.replace(",", ";")}"\n')
             exception_ = True
             exception_file.flush()
 
     if not exception_:
         print(f"Successfully evaluated index {index+1}", flush=True)
 
-    repetition_results["coherence"] /= coherence_count
-    repetition_results["fluency"] /= fluency_count
-    repetition_results["relevance"] /= relevance_count
-    repetition_results["consistency"] /= consistency_count
+    repetition_results["result"] /= count
 
     results.loc[index] = repetition_results
 
 results.to_csv(
     os.path.join(
-        csv_files_path, f"{model_name.split(':')[1]}_{evaluation_type}_results.csv"
+        csv_files_path,
+        f"{model_version}_{metric}_{evaluation_type}_results.csv",
     )
 )
 print("Results saved to csv file.", flush=True)
