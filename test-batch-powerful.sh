c8f6962972081f6ee6ce908ff446d316b06ab31b
tenacke <emrekilic1010@gmail.com>
2025-03-26 20:21:57 +0300

    added different sbatch scripts

---
+++ test-batch-powerful.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH --job-name=slurm-test
+#SBATCH --container-image ghcr.io\#tenacke/llm-evaluator
+#SBATCH --container-mounts=/users/emre.kilic/llm-evaluator/csv:/opt/llm-evaluator/csv,/users/emre.kilic/llm-evaluator/logs:/opt/llm-evaluator/logs
+#SBATCH --time=08:00:00
+#SBATCH --gpus=1
+#SBATCH --cpus-per-gpu=8
+#SBATCH --mem-per-gpu=40G
+
+source /opt/llm-evaluator/venv/bin/activate
+mkdir -p /opt/llm-evaluator/logs
+git fetch --all && \
+git checkout container && \
+git pull origin container
+ollama serve > /opt/llm-evaluator/logs/ollama.log 2>&1 < /dev/null &
+echo "Waiting for the server to start..."
+sleep 5
+echo "Server is up! Proceeding with the next command."
+# bash models/v3/model-compose.sh
+ollama pull llama3.1:70b && \
+python3 models/v3/model_test.py && \
+python3 tester.py llama3.1:70b powerful 3 coherence && \
+python3 tester.py llama3.1:70b powerful 3 fluency && \
+python3 tester.py llama3.1:70b powerful 3 relevance && \
+python3 tester.py llama3.1:70b powerful 3 consistency 
+
+
+
