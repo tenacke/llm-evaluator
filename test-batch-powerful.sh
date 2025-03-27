39160685bc5a5f2a661566d0fc15e529a82f8076
Bilge Kaan Güneyli <bilgekaanguneyli@gmail.com>
2025-03-27 18:00:18 +0300

    update test-batch-powerful

--- test-batch-powerful.sh
+++ test-batch-powerful.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=powerful-test
-#SBATCH --container-image ghcr.io\#tenacke/llm-evaluator:lastest-container
+#SBATCH --container-image ghcr.io\#tenacke/llm-evaluator:latest-container
 #SBATCH --container-mounts=/users/bilge.guneyli/llm-evaluator/outputs:/opt/llm-evaluator/outputs,/users/bilge.guneyli/llm-evaluator/logs:/opt/llm-evaluator/logs
 #SBATCH --time=08:00:00
 #SBATCH --cpus-per-task=8
