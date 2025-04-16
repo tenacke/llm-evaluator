ef64d6293ea80e9a9d3b5d61ba110db24ebcc407
Bilge Kaan Güneyli <bilgekaanguneyli@gmail.com>
2025-04-17 00:41:14 +0300

    update sh

--- test-pairwise.sh
+++ test-pairwise.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 #SBATCH --job-name=average-test
 #SBATCH --container-image ghcr.io\#tenacke/llm-evaluator:latest-container
-#SBATCH --container-mounts=/users/emre.kilic/llm-evaluator/outputs:/opt/llm-evaluator/outputs,/users/emre.kilic/llm-evaluator/logs:/opt/llm-evaluator/logs
+#SBATCH --container-mounts=/users/bilge.guneyli/llm-evaluator/output:/opt/llm-evaluator/output,/users/bilge.guneyli/llm-evaluator/logs:/opt/llm-evaluator/logs
 #SBATCH --time=08:00:00
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=4G
 
 source /opt/llm-evaluator/venv/bin/activate
+git fetch --all && \
+git checkout pairwise-emre && \
+git pull origin pairwise-emre
 
 python3 tester.py llama3.1:70b random_200 3 && \
 python3 tester.py llama3.1:70b vicukoala_100 3 && \
