0ee8243740a1a1b490ce2029e10ef11e3adf92ba
Bilge Kaan Güneyli <bilgekaanguneyli@gmail.com>
2025-04-18 14:14:25 +0300

    new prompt

--- correlation.py
+++ correlation.py
@@ -24,18 +24,13 @@ if not os.path.exists(test_path):
     print(f"Path {test_path} does not exist")
     sys.exit(1)
 
-csv_dir_path = os.path.join(os.path.dirname(__file__), "csv")
-datasets_path = os.path.join(os.path.dirname(__file__), "datasets")
+base_file = os.path.join(base_path, f"{datafile_name}.csv")
+print(f"Base file: {base_file}")
+base_df = pd.read_csv(base_file)
 
-base_df = pd.read_csv(
-    os.path.join(base_path, f"{datafile_name}.csv"),
-    # usecols=["gold_label", "result"],
-)
-
-test_df = pd.read_csv(
-    os.path.join(test_path, f"{model_name}_{datafile_name}_pw.csv"),
-    # usecols=["result"],
-)
+test_file = os.path.join(test_path, f"{model_name}_{datafile_name}_pw.csv")
+print(f"Test file: {test_file}")
+test_df = pd.read_csv(test_file)
 
 one_one = 0
 one_two = 0
@@ -60,36 +55,8 @@ for i in range(len(base_df)):
 true_count = one_one + two_two
 false_count = one_two + two_one
 
-# # Calculate precision, recall, and F-score
-# precision = (
-#     true_positive / (true_positive + false_positive)
-#     if (true_positive + false_positive) > 0
-#     else 0
-# )
-# recall = (
-#     true_positive / (true_positive + false_negative)
-#     if (true_positive + false_negative) > 0
-#     else 0
-# )
-# f_score = (
-#     2 * (precision * recall) / (precision + recall) if (precision + recall) > 0 else 0
-# )
-
-# print(f"Evaluation of {nli_model_name} using {model_name}")
-# print("\nConfusion Matrix:")
-# print("-" * 41)
-# print("|" + " " * 17 + "|     Predicted       |")
-# print("|" + " " * 17 + "| Positive | Negative |")
-# print("|" + "-" * 39 + "|")
-# print(f"| Actual Positive | {true_positive:8} | {false_negative:8} |")
-# print(f"| Actual Negative | {false_positive:8} | {true_negative:8} |")
-# print("-" * 41)
-# print(f"\nPrecision: {precision:.4f}")
-# print(f"Recall: {recall:.4f}")
-# print(f"F-score: {f_score:.4f}")
-
-print(f"1: {one_one + one_two}")
-print(f"2: {two_one + two_two}")
+print(f"model chose 1: {two_one + one_one}")
+print(f"model chose 2: {one_two + two_two}")
 print(f"1-2: {one_two}")
 print(f"2-1: {two_one}")
 print(f"1-1: {one_one}")
