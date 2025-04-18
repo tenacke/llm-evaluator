c911fb4424315e9b13931e31628c896298b3487a
Bilge Kaan Güneyli <bilgekaanguneyli@gmail.com>
2025-04-18 14:36:47 +0300

    llama nli model

--- test-batch.sh
+++ test-batch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=test-nli
+#SBATCH --job-name=nli
 #SBATCH --container-image ghcr.io\#tenacke/llm-evaluator:latest-container
 #SBATCH --container-mounts=/users/bilge.guneyli/llm-evaluator/output:/opt/llm-evaluator/output,/users/bilge.guneyli/llm-evaluator/logs:/opt/llm-evaluator/logs
 #SBATCH --time=24:00:00
@@ -13,9 +13,10 @@ git commit -m "Update entrypoint.sh" && \
 git checkout kaan-nli && \
 git pull origin kaan-nli
 
-python3 tester.py llama3.1:70b roberta 3 && \
-python3 tester.py llama3.1:70b deberta 3 && \
-python3 tester.py llama3.1:70b random 3 
+python3 tester.py llama3.1:70b llama3.1:70b 3 #&& \
+# python3 tester.py llama3.1:70b roberta 3 && \
+# python3 tester.py llama3.1:70b deberta 3 && \
+# python3 tester.py llama3.1:70b random 3 
 
 
 
