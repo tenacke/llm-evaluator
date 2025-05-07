78cd78ee5aa7de569a531248e702e76f6adf6a9a
tenacke <emrekilic1010@gmail.com>
2025-05-07 17:46:55 +0300

    make this branch only contains package

--- llm_evaluator/llm_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.evaluation_options, name='evaluation_options'),
-    path('get-random-line/', views.get_random_line, name='get_random_line'),
-    path('get-file-content/<str:type>/<str:storyId>/', views.get_file_content, name='get_file_content'),
-    path('get-random-nli/', views.get_random_nli, name='get_random_nli'),
-]
