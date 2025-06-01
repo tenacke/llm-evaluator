5d4caa19a670ddc64f8c1585aa61f140619e3d8d
tenacke <emrekilic1010@gmail.com>
2025-06-01 16:36:48 +0300

    fix allowed hosts

--- llm_evaluator/settings.py
+++ llm_evaluator/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-05j^d8e4lk6n7!bs50ny$omqg9yf%b(*7xl!*$afoypu6%ryu^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
